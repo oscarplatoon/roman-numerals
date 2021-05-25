2ff211471831ce7a4c166d8cd2dee9ee5292d74d
Smattonline <57063258+Smattonline@users.noreply.github.com>
2021-05-25 13:45:03 -0700

    Musa and Jonas' Roman Numerals Challenge

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,31 @@
-exports.toRoman = function(num) {
+function toRoman(x) {
+  let romanNum = "";
+  let nums = {
+    M: 1000,
+    CM: 900,
+    D: 500, 
+    CD: 400,
+    C: 100,
+    XC: 90, 
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
 
+  for (romans in nums) {
+    while (x >= nums[romans]) {
+      romanNum += romans;
+      x -= nums[romans];
+    }
+  }
+
+  return romanNum;
 };
+
+console.log(toRoman(970))// === 'CMLXX')
+console.log(toRoman(680))// === 'DCLXXX')
+console.log(toRoman(500))// === 'D')
