64afa0facce103afca75583fd596070268e1381d
stesun2 <ss27@illinois.edu>
2021-05-25 13:55:50 -0500

    Stephen Sun & Shawn Kiernan completed romanNumberals

--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -3,3 +3,10 @@ var rn = require("./romanNumerals");
 console.log(rn.toRoman(1) === 'I');
 console.log(rn.toRoman(3) === 'III');
 console.log(rn.toRoman(4) === 'IV');
+console.log(rn.toRoman(9) === 'IX');
+console.log(rn.toRoman(14) === 'XIV');
+console.log(rn.toRoman(44) === 'XLIV');
+console.log(rn.toRoman(944) === 'CMXLIV');
+console.log(rn.toRoman(1000) === 'M');
+
+console.log(rn.toRoman(2000) === 'MM');
