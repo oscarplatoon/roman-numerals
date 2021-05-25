5c352e3f3425553306cbc7d34a8d735cee29e7a7
Kyle Gunter <kylewgunter@gmail.com>
2021-05-25 14:02:31 -0500

    Sarah and Kyle pt1

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,36 @@
 exports.toRoman = function(num) {
-
+  let ansStr = '';
+  do {
+    if(num >= 1000){
+      num -= 1000;
+      ansStr += 'M'
+    }
+    else if (num >= 500){
+      num -= 500;
+      ansStr += 'D'
+    }
+    else if (num >= 100){
+      num -= 100;
+      ansStr += 'C'
+    }
+    else if (num >= 50){
+      num -= 50;
+      ansStr += 'L'
+    }
+    else if (num >= 10){
+      num -= 10;
+      ansStr += 'X'
+    }
+    else if (num >= 5){
+      num -= 5;
+      ansStr += 'V'
+    }
+    else {
+      num -= 1;
+      ansStr += 'I'
+    }
+  } while (num > 0);
+  console.log(ansStr)
 };
+
+
