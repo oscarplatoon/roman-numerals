6444829a1787f130eb765b9797d63fd7473b2e66
Ankur Shah <ankur@codeplatoon.org>
2021-05-26 10:51:25 -0500

    solved Part 1: Lazy Roman Numeral

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,58 @@
-exports.toRoman = function(num) {
+// 1. Write a method TO_ROMAN, TO_ROMAN takes in INPUT_NUMBER (an arabic number)
+// 2. Create a OUTPUT string, set to ''
+// 3. Create a ROMAN_NUMERAL_TO_ARABIC_MAP that includes roman numerals as keys, arabic numbers as values
+// 4. Iterate over ROMAN_NUMERAL_TO_ARABIC_MAP, keep track of ROMAN_NUMERAL and ARABIC_NUMBER
+// 5. Set EVENLY_DIVISIBLE_TIMES = INPUT_NUMBER / ARABIC_NUMBER:
+// 6. If EVENLY_DIVISIBLE_TIMES >= 1
+  // 6a. Append ROMAN_NUMERAL to OUTPUT EVENLY_DIVISIBLE_TIMES
+  // 6b. Subtract ARABIC_NUMBER from INPUT_NUMBER EVENLY_DIVISIBLE_TIMES
+// 7. Return OUTPUT
 
+
+// I -> 1
+// V -> 5
+// X -> 10
+// L -> 50
+// C -> 100
+// D -> 500
+// M -> 1000
+
+
+const decimalToRoman = [
+    { 1000:   "M"},
+    { 500:    "D"},
+    { 100:    "C"},
+    { 50:     "L"},
+    { 10:     "X"},
+    { 5:      "V"},
+    { 1:      "I"}
+]
+
+exports.toRoman = function(num) {
+    let romanStr = ""
+
+    for (let obj of decimalToRoman) {
+        let inc = Object.keys(obj)[0]
+        let roman = obj[inc]
+        //console.log(decimal)
+        //console.log(obj[decimal])
+
+        let result = Math.floor(num / inc) 
+        //console.log(result)
+
+        for (let i = 0; i < result; i++) {
+            romanStr += roman
+        }
+
+        //console.log(romanStr)
+        num = num % inc
+
+        if (num === 0) {
+            break
+        }
+    } 
+    console.log(romanStr)
+    return romanStr 
 };
+
+//exports.toRoman(683)
--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -2,4 +2,8 @@ var rn = require("./romanNumerals");
 
 console.log(rn.toRoman(1) === 'I');
 console.log(rn.toRoman(3) === 'III');
-console.log(rn.toRoman(4) === 'IV');
+console.log(rn.toRoman(4) === 'IIII');
+console.log(rn.toRoman(300) === 'CCC');
+console.log(rn.toRoman(123) === 'CXXIII');
+console.log(rn.toRoman(2400) === 'MMCCCC');
+console.log(rn.toRoman(939) === 'DCCCCXXXVIIII');
