ab42ee07b194dbe38cba2c3ec4674fb1e79fe2d2
jakewhite5 <jakewhite5@yahoo.com>
2021-05-26 09:52:24 -0700

    completed in python

--- python/roman_numerals_spec.py
+++ python/roman_numerals_spec.py
@@ -3,4 +3,5 @@ from roman_numerals import to_roman
 print(to_roman(1) == 'I')
 print(to_roman(3) == 'III')
 print(to_roman(4) == 'IV')
+print(to_roman(939) == 'CMXXXIX')
 # add tests to cover different edge cases
