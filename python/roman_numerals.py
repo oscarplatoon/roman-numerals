96c394943ba768249e9721af6252cf544db5085b
RenoAverill <reno_averill@yahoo.com>
2021-05-26 15:28:28 -0600

    Reno's solution lazy roman_numerals.py

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,19 @@
 def to_roman(num):
-    # write your code here!
+    result = ''
+    roman_list = [
+        [1000, 'M'],
+        [500, 'D'],
+        [100, 'C'],
+        [50, 'L'],
+        [10, 'X'],
+        [5, 'V'],
+        [1, 'I']
+    ]
+    for index, value in enumerate(roman_list):
+        while num >= roman_list[index][0]:
+           result += roman_list[index][1]
+           num -= value[0]
+    return result
+    
+
+print(to_roman(4))
