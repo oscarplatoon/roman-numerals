7bdf130528fbada6e77ffb5766d3a9151e206072
vzbrunstad <vzbrunstad@msn.com>
2021-05-26 16:08:51 -0500

    Vince B Roman Numeral py and js complete

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,33 @@
 def to_roman(num):
-    # write your code here!
+    roman_dict = {
+
+        'M': 1000,
+    'CM' : 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
+        
+    } 
+
+    output = ""
+    for x in roman_dict:
+       #print (roman_dict[x])
+        while (num>=roman_dict[x]):
+            output = output + x
+            num = num - roman_dict [x]
+
+    return output
+to_roman(60)
+        
+        
+        
+        
+    
