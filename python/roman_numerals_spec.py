ea119de82e4d80e68fd8402dfc211cd0098b5038
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-05-26 15:00:30 -0500

    Shawn's python solution

--- python/roman_numerals_spec.py
+++ python/roman_numerals_spec.py
@@ -1,6 +1,13 @@
 from roman_numerals import to_roman
+from roman_numerals import to_roman_modern
 
 print(to_roman(1) == 'I')
 print(to_roman(3) == 'III')
-print(to_roman(4) == 'IV')
+print(to_roman(4) == 'IIII')
+print(to_roman(505) == "DV")
+print(to_roman(739) == "DCCXXXVIIII")
 # add tests to cover different edge cases
+
+print(to_roman_modern(4) == 'IV')
+print(to_roman_modern(739) == "DCCXXXIX")
+print(to_roman_modern(944) == "CMXLIV")
