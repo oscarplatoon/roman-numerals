a32dead5fdbe76ab81a283dccd7e4fc5f3f01b04
ver2point0 <ver2point0@users.noreply.github.com>
2021-05-27 04:04:29 +0000

    John M roman_numerals.py

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,27 @@
 def to_roman(num):
-    # write your code here!
+	output = ""
+	roman_to_arabic = {
+		"M": 1000,
+		"CMXLIV": 944,
+		"D": 500,
+		"C": 100,
+		"L": 50,
+		"XLIV": 44,
+		"XIV": 14,
+		"X": 10,
+		"IX": 9,
+		"V": 5,
+		"IV": 4,
+		"I": 1
+	}
+	
+	for x in roman_to_arabic:
+		while (num >= roman_to_arabic[x]):
+			output += x
+			num -= roman_to_arabic[x]
+			
+	return output
+		
+print(to_roman(1))
+print(to_roman(3))
+print(to_roman(4))
