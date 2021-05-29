39a0809a88447df6dcdb0c75f8c8f3be70abe73e
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-05-29 10:45:29 -0400

    Python and unit test solution

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,30 @@
 def to_roman(num):
-    # write your code here!
+    # Check input for correctness
+    if num is None:
+        return None
+    elif not isinstance(num, int):
+        return None
+
+    int_roman_list = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I"),
+    ]
+    result = ""
+    for (arabic, roman) in int_roman_list:
+        (factor, num) = divmod(num, arabic)
+        result += roman * factor
+
+    return result
+
+print(to_roman(1))
--- python/roman_numerals_spec.py
+++ python/roman_numerals_spec.py
@@ -1,6 +1,29 @@
+import unittest
 from roman_numerals import to_roman
 
-print(to_roman(1) == 'I')
-print(to_roman(3) == 'III')
-print(to_roman(4) == 'IV')
-# add tests to cover different edge cases
+class TestRoman(unittest.TestCase):
+    """
+    When you call to_roman with a non int parameter you get back None
+    """
+
+    def test_returns_none(self):
+        self.assertTrue(to_roman("Hi!") == None)
+
+    """
+    When you call to_roman you get back proper modern roman conversion:
+    """
+
+    def test_roman_function(self):
+        self.assertEqual(to_roman(1), "I")
+        self.assertEqual(to_roman(3), 'III')
+        self.assertEqual(to_roman(4),  'IV')
+        self.assertEqual(to_roman(2499), 'MMCDXCIX')
+        
+if __name__ == '__main__':
+    unittest.main()
+
+
+# print(to_roman(1) == 'I')
+# print(to_roman(3) == 'III')
+# print(to_roman(4) == 'IV')
+# # add tests to cover different edge cases
